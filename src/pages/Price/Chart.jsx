02eeef38a56751024a11dbac541e72c51ac792c5
Abolfazl Abdali <abolfazlandali0@gmail.com>
2022-03-06 14:04:12 +0330

    feat: price page

---
+++ src/pages/Price/Chart.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { Button, Text, VStack, HStack, Flex } from "@chakra-ui/react";
+
+import { AdvancedRealTimeChart } from "react-ts-tradingview-widgets";
+
+function ChartCom({ price, currency, symbol }) {
+  return (
+    <VStack w="100%" h="400px" sx={{ "& > *": { w: "100%" } }} bg="white">
+      <AdvancedRealTimeChart
+        theme="light"
+        autosize
+        hide_legend
+        hide_top_toolbar
+        hide_side_toolbar
+        style={3}
+        symbol={symbol + currency}
+      ></AdvancedRealTimeChart>
+    </VStack>
+  );
+}
+
+export default ChartCom;
