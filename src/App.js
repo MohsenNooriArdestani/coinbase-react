1029c0fcb88ea8e4fc7118925454103412c63c76
Abolfazl Abdali <abolfazlandali0@gmail.com>
2022-03-07 19:21:22 +0330

    fix: router basename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppRoutes />
     </BrowserRouter>
   );
