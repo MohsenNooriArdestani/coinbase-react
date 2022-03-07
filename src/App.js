808308caf386524c42c917f79e8a991387f72296
Abolfazl Abdali <abolfazlandali0@gmail.com>
2022-03-07 19:21:22 +0330

    fix: router basename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={"/coinbase-react"}>
       <AppRoutes />
     </BrowserRouter>
   );
